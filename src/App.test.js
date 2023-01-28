00760e75bd6ce271736153673077fb26ddd7e65b
Olivier Bussier <olivier@bussier.fr>
2023-01-29 00:50:23 +0100

    Prépa des tests jest avec routeur

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// app.test.js
+import {screen, waitFor } from '@testing-library/react'
+import App from './App'
+import React from 'react'
+import '@testing-library/jest-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  let linkElement = screen.getByText(/Chez vous, partout et ailleurs/i);
-  expect(linkElement).toBeInTheDocument();
+import renderWithRouter from './testUtils'
 
-});
+test('full app rendering/navigating', async () => {
+  const {user} = renderWithRouter(<App />)
+  expect(screen.getByText(/Chez vous, partout et ailleurs/i)).toBeInTheDocument()
+
+  const button = screen.getByText(/A Propos/i)
+
+  await user.click(button)
+  // console.log(toto)
+  await waitFor(() => expect(screen.getByText(/La sécurité est la priorité de Kasa/i)).toBeInTheDocument())
+  console.log(button)
+
+})
